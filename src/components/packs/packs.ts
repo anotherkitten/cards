607c2c570a320a676065a4b5cf056fea7fbd3da3
Ellie Larson <ellie@polklarson.com>
2026-06-10 01:10:53 -0500

    Auto-generated commit

--- src/components/packs/packs.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-packs',
-  imports: [],
-  templateUrl: './packs.html',
-  styleUrl: './packs.css',
-})
-export class Packs {}
